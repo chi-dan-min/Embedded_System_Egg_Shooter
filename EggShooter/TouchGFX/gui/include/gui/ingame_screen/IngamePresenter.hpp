5c4410477c51662ba51fe6e4ff2413bc10413b3c
kien04034 <nguyenkientk123@gmail.com>
2025-06-25 23:32:52 +0700

    Test xử lý sự kiện ban đầu

---
+++ EggShooter/TouchGFX/gui/include/gui/ingame_screen/IngamePresenter.hpp
@@ -0,0 +1,36 @@
+#ifndef INGAMEPRESENTER_HPP
+#define INGAMEPRESENTER_HPP
+
+#include <gui/model/ModelListener.hpp>
+#include <mvp/Presenter.hpp>
+
+using namespace touchgfx;
+
+class IngameView;
+
+class IngamePresenter : public touchgfx::Presenter, public ModelListener
+{
+public:
+    IngamePresenter(IngameView& v);
+
+    /**
+     * The activate function is called automatically when this screen is "switched in"
+     * (ie. made active). Initialization logic can be placed here.
+     */
+    virtual void activate();
+
+    /**
+     * The deactivate function is called automatically when this screen is "switched out"
+     * (ie. made inactive). Teardown functionality can be placed here.
+     */
+    virtual void deactivate();
+
+    virtual ~IngamePresenter() {}
+
+private:
+    IngamePresenter();
+
+    IngameView& view;
+};
+
+#endif // INGAMEPRESENTER_HPP
